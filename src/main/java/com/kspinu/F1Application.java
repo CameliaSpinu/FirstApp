c6d257c1130d2d99e3b6251b0fe57e316feff27c
Camelia Spinu <kspinudev@gmail.com>
2022-01-11 21:16:33 +0200

    Init commit

---
+++ src/main/java/com/kspinu/F1Application.java
@@ -0,0 +1,15 @@
+package com.kspinu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(F1Application.class, args);
+
+    }
+
+}
