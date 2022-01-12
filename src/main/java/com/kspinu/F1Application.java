0356aa3430db6f3bb44d990d7865de4686f87898
Nicolae Spinu <11spinic@gmail.com>
2022-01-12 20:17:36 +0200

    Added H2db, added welcome message

--- src/main/java/com/kspinu/F1Application.java
+++ src/main/java/com/kspinu/F1Application.java
@@ -9,7 +9,9 @@ public class F1Application {
     public static void main(String[] args) {
 
         SpringApplication.run(F1Application.class, args);
-
+        System.out.println("======================================");
+        System.out.println("Welcome Camelia!");
+        System.out.println("======================================");
     }
 
 }
