721acfacb9d090fb6725d7841492aaf09e05b32f
Camelia Spinu <kspinudev@gmail.com>
2022-01-31 19:56:55 +0200

    Added update endpoint

--- src/main/java/com/kspinu/controller/EmployeeController.java
+++ src/main/java/com/kspinu/controller/EmployeeController.java
@@ -1,19 +1,14 @@
 package com.kspinu.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import com.kspinu.model.Employee;
 import com.kspinu.repository.EmployeeRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -27,28 +22,43 @@ public class EmployeeController {
     }
 
     @PostMapping()
-    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
+    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
         return new ResponseEntity<Employee>(employeeRepository.save(employee), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public List<Employee> getAllEmployees(){
+    public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long employeeId){
-        return new ResponseEntity<Employee>(employeeRepository.getById(employeeId), HttpStatus.OK);
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long employeeId) {
+        Optional<Employee> employee1 = employeeRepository.findById(employeeId);
+
+        if (employee1.isPresent()) {
+            return new ResponseEntity<Employee>(employee1.get(), HttpStatus.OK);
+        } else {
+            throw new NoSuchElementException("Employee ID: " + employeeId + "  doesn't exist");
+        }
     }
 
     @PutMapping("{id}")
     public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long id
-            ,@RequestBody Employee employee){
-        return new ResponseEntity<Employee>(employeeRepository.findById(employee, id), HttpStatus.OK);
+            , @RequestBody Employee employee) {
+
+        Employee existingEmployee = employeeRepository.findById(id).orElseThrow(
+                () -> new NoSuchElementException("Employee ID: " + id));
+
+        existingEmployee.setFirstName(employee.getFirstName());
+        existingEmployee.setLastName(employee.getLastName());
+        existingEmployee.setEmail(employee.getEmail());
+        employeeRepository.save(existingEmployee);
+        return new ResponseEntity<>(existingEmployee, HttpStatus.OK);
     }
 
+
     @DeleteMapping("{id}")
-    public ResponseEntity<String> deleteEmployee(@PathVariable("id") long id){
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") long id) {
 
         employeeRepository.deleteById(id);
 
